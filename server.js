469fb9428e149d942a08eed1f3e50240337b100d
afox226293 <andrew.fox988@gmail.com>
2017-09-23 18:55:02 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+// middleware
+app.use((request, response, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${request.method} ${request.url}`;
+    
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    
+    next();
+});
+
+//app.use((request, response, next) => {
+//    response.render('maintenance.hbs');
+//});
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getYear', () => {
+   return new Date().getFullYear(); 
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to this webpage',
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+})
+
+app.get('/bad', (request, response) => {
+    response.send({
+        errorMessage: 'Error: Bad request'
+    });
+});
+
+app.listen(3000);
